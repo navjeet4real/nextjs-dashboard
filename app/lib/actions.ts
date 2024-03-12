9e49d72df608e8c62417cb70bbc3c0b4ec2c46ff
navjeet4real <navjeetkajal.2594@gmail.com>
2024-03-12 12:57:01 +0530

    form validation using zod

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,46 +7,83 @@ import { redirect } from "next/navigation";
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['paid', 'pending']),
+    customerId: z.string({
+        invalid_type_error: 'Customer ID is required',
+    }),
+    amount: z.coerce.number().gt(0, { message: 'Amount must be greater than $0' }),
+    status: z.enum(['paid', 'pending'], {
+        invalid_type_error: 'Status must be paid or pending',
+    }),
     date: z.string(),
 })
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
+export type State = {
+    errors?: {
+        customerId?: string[],
+        amount?: string[],
+        status?: string[],
+    },
+    message: string | null
+}
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+    // Validate form using Zod
+    const validateFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validateFields.success) {
+        return {
+            errors: validateFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice',
+        }
+    }
+
+    // Prepare for data insertion into the database
+    const { customerId, amount, status } = validateFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
+    // Insert the data into the database
     try {
         await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
     } catch (error) {
+        // If a database error occurs, return a more specific error.
         return {
             message: 'Database Error: Failed to Create Invoice.',
         };
     }
 
+
+    // Revalidate the cache for the invoices page and redirect the user.
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export async function updateInvoice( id: string, prevState: State, formData: FormData) {
+    const validateFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if(!validateFields.success){
+        return {
+            errors: validateFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice',
+        }
+    }
+
+    const { customerId, amount, status } = validateFields.data;
     const amountInCents = amount * 100;
+
+    // Update the inoice from the database using Id.
     try {
         await sql`
     UPDATE invoices
@@ -55,23 +92,28 @@ export async function updateInvoice(id: string, formData: FormData) {
   `;
 
     } catch (error) {
+        // If a database error occurs, return a more specific error.
         return {
             message: 'Database Error: Failed to Update Invoice.',
         };
     }
+
+    // Revalidate the cache for the invoices page and redirect the user.
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
+    // Delete the invoice from the database
     try {
         await sql`
     DELETE FROM invoices
     WHERE id = ${id}`
+        // Revalidate the cache for the invoices page.
         revalidatePath('/dashboard/invoices');
         return { message: 'Deleted Invoice.' };
     } catch (error) {
+        // If a database error occurs, return a more specific error.
         return {
             message: 'Database Error: Failed to Delete Invoice.',
         };
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,6 +10,7 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateInvoice } from '@/app/lib/actions';
+const { useFormState } = require('react-dom');
 
 export default function EditInvoiceForm({
   invoice,
@@ -19,8 +20,15 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
+  const initialState = {
+    message: null,
+    errors: {},
+  };
+  const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
+  console.log(state, 'state');
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -33,6 +41,7 @@ export default function EditInvoiceForm({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
+              aria-describedby='customer-error'
             >
               <option value="" disabled>
                 Select a customer
@@ -46,6 +55,13 @@ export default function EditInvoiceForm({
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
         </div>
+        {state.errors?.customerId && state.errors.customerId.map((error: string) => {
+          return (
+            <p className='mt-1 text-sm text-red-500' key={error}>
+              {error}
+            </p>
+          )
+        })}
 
         {/* Invoice Amount */}
         <div className="mb-4">
@@ -62,11 +78,19 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby='amount-error'
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
         </div>
+        {state.errors?.amount && state.errors.amount.map((error: string) => {
+          return (
+            <p className='mt-1 text-sm text-red-500' key={error}>
+              {error}
+            </p>
+          )
+        })}
 
         {/* Invoice Status */}
         <fieldset>
@@ -83,6 +107,7 @@ export default function EditInvoiceForm({
                   value="pending"
                   defaultChecked={invoice.status === 'pending'}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby='status-error'
                 />
                 <label
                   htmlFor="pending"
@@ -99,6 +124,7 @@ export default function EditInvoiceForm({
                   value="paid"
                   defaultChecked={invoice.status === 'paid'}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby='status-error'
                 />
                 <label
                   htmlFor="paid"
@@ -110,6 +136,14 @@ export default function EditInvoiceForm({
             </div>
           </div>
         </fieldset>
+        {state.errors.status && state.errors.status.map((error: string) => {
+          return (
+            <p className='mt-1 text-sm text-red-500' key={error}>
+              {error}
+            </p>
+          )
+        })}
+        {state.message && <p className='mt-1 text-sm text-red-500'>{state.message}</p>}
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
